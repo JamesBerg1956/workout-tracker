67458426d7866b8353b650acb94451b359a658cf
James Berg <james.berg38@yahoo.com>
2020-04-23 18:12:16 -0500

    added pseudocode to models/workout.js

---
+++ models/workout.js
@@ -0,0 +1,22 @@
+// TODO: import mongoose
+
+// TODO: create Schema object from mongoose
+
+// TODO: create WorkoutSchema
+
+    // TODO: define day
+
+        // type Date
+        // default: Date.now
+
+    // TODO: define exercises array
+
+        //type: Schema.Types.ObjectId,
+        //unique: true,
+        //// reference Exercise model
+        //ref: "Exercise"
+
+
+// create Workout object with mongoose and WorkoutSchema
+
+// export module
