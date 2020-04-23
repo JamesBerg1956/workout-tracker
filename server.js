1b9abf177b7a1d57ea144efed6aaf61b0a7892d0
James Berg <james.berg38@yahoo.com>
2020-04-23 17:42:42 -0500

    added pseudocode to server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+// TODO: import express
+
+// TODO: import mongojs
+
+// TODO: import morgan
+
+// TODO: import mongoose
+
+// TODO: create PORT
+
+// TODO: create app object form express
+
+// TODO: setup server middleware with app object
+
+// TODO: set app object to use public folder
+
+// TODO: connect to mogodb dataase usin mongoose
+
+// TODO: create collections name array
+
+// TODO: create db object from mongo with database naem and collections name array
+
+// TODO: error handing for db object errors
+
+
+// TODO: create routes
+
+
+// TODO: set app object to listen on PORT
+
