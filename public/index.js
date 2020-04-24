f66ec65379596ed3488bac146ff268204d694c9e
James Berg <james.berg38@yahoo.com>
2020-04-24 08:11:24 -0500

    commented public/index.js

--- public/index.js
+++ public/index.js
@@ -1,13 +1,27 @@
 init();
 
+// START init function
 async function init() {
+
+  // if the url querysting contains does not contain '='
   if (location.search.split("=")[1] === undefined) {
+
+    // get a json object of the most recent workout
     const workout = await API.getLastWorkout();
+
+    // START if getLastWorkout() returned a json object
     if (workout) {
+      // append ?id=objectId to current url querystring
       location.search = "?id=" + workout._id;
-    } else {
+    }
+    // END if getLastWorkout() returned a json object 
+    // START else getLastWorkout() did not return a json object
+    else {
+      // hide the Continue Workout button
       document.querySelector("#continue-btn").classList.add("d-none")
     }
+    // END else getLastWorkout() did not return a json object
+
   }
 }
-
+// END init function
