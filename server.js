3e7a32de1c6cd5932bc10c193c09f20bca277e98
James Berg <james.berg38@yahoo.com>
2020-04-24 18:14:21 -0500

    added exercise html route

--- server.js
+++ server.js
@@ -28,10 +28,17 @@ app.use(express.static("public"));
 // connect to mogodb database using mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
 
-// /stats html GET route
+// /stats html GET HTML route
 app.get("/stats", function(req, res){
     res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
+
+// /exercise GET HTML route
+app.get("/exercise", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+// TODO: /exercise?id=objectID HTML route
 
 // /api/workouts api GET route
 app.get("/api/workouts", function(req, res) {
@@ -44,8 +51,7 @@ app.get("/api/workouts", function(req, res) {
     });
 });
 
-// TODO: /api/workouts/range GET route
-// /api/workouts api GET route
+// /api/workouts/range GET route
 app.get("/api/workouts/range", function(req, res) {
     db.Workout.find({})
     .then(function(dbWorkouts){
